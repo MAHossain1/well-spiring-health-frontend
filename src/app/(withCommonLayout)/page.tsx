b2efb021e77887742382731c24a8d0655b622500
MAHossain1 <mahpro110@gmail.com>
2024-09-03 18:04:35 +0600

    Added TopRatedDoctor and WhyUs section on homepage.

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,5 +1,7 @@
 import HeroSection from '@/components/UI/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/UI/HomePage/Specialist/Specialist';
+import TopRatedDoctors from '@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors';
+import WhyUs from '@/components/UI/HomePage/WhyUs/WhyUs';
 import { Button } from '@mui/material';
 import Image from 'next/image';
 
@@ -8,6 +10,8 @@ export default function Home() {
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
+      <WhyUs />
     </>
   );
 }
