ae3fda262e339c1e01d5a64eff627cc8208448a6
MAHossain1 <mahpro110@gmail.com>
2024-11-10 21:29:29 +0600

    Deleted specialty done.

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,29 +1,64 @@
 'use client';
 
-import { Box, Button, Stack, TextField } from '@mui/material';
+import {
+  useDeleteSpecialtyMutation,
+  useGetAllSpecialtiesQuery,
+} from '@/redux/api/specialtiesApi';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, IconButton, Stack, TextField } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Image from 'next/image';
 import { useState } from 'react';
 import SpecialtyModal from './components/SpecialtyModal';
-import { useGetAllSpecialtiesQuery } from '@/redux/api/specialtiesApi';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { DataGridPro } from '@mui/x-data-grid-pro';
-import Image from 'next/image';
+import { toast } from 'sonner';
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const { data, isLoading } = useGetAllSpecialtiesQuery({});
 
+  const [deleteSpecialty] = useDeleteSpecialtyMutation();
+
+  const handleDelete = async (id: string) => {
+    console.log(id);
+    try {
+      const res = await deleteSpecialty(id);
+      console.log(res);
+      toast.success('Specialty deleted done.');
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
   const columns: GridColDef[] = [
-    { field: 'title', headerName: 'Title', width: 260 },
+    { field: 'title', headerName: 'Title', flex: 1 },
     {
       field: 'icon',
       headerName: 'Icon',
-      width: 160,
+      flex: 1,
       renderCell: ({ row }) => {
         return (
           <Box mt={1}>
             <Image src={row.icon} alt="icon" width={25} height={25} />
           </Box>
+        );
+      },
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      flex: 1,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: ({ row }) => {
+        return (
+          <IconButton
+            onClick={() => handleDelete(row.id)}
+            aria-label="delete"
+            size="large"
+          >
+            <DeleteIcon fontSize="inherit" />
+          </IconButton>
         );
       },
     },
