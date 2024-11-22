254ba0d5977058012441dc1e07430699b68bbc04
MAHossain1 <mahpro110@gmail.com>
2024-11-22 19:09:26 +0600

    doctor profile updation done.

--- src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
@@ -6,12 +6,16 @@ import {
   useUpdateMYProfileMutation,
 } from '@/redux/api/myProfile';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { Box, Container } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import Image from 'next/image';
 import DoctorInformation from './components/DoctorInformation';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { useState } from 'react';
+import ProfileUpdateModal from './components/ProfileUpdateModal';
 
 const DoctorProfilePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { data, isLoading } = useGetMYProfileQuery(undefined);
   const [updateMYProfile, { isLoading: loading }] =
     useUpdateMYProfileMutation();
@@ -31,43 +35,57 @@ const DoctorProfilePage = () => {
   //   console.log(data);
 
   return (
-    <Container sx={{ mt: 4 }}>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12, md: 8 }}>
-          <Box
-            sx={{
-              height: 300,
-              width: '100%',
-              overflow: 'hidden',
-              borderRadius: 1,
-            }}
-          >
-            <Image
-              height={300}
-              width={400}
-              src={data?.profilePhoto}
-              alt="User Photo"
-            />
-          </Box>
-          <Box my={3}>
-            {loading ? (
-              <p>Uploading...</p>
-            ) : (
-              <AutoFileUploader
-                name="file"
-                label="Choose Your Profile Photo"
-                icon={<CloudUploadIcon />}
-                onFileUpload={fileUploadHandler}
-                variant="text"
+    <>
+      <ProfileUpdateModal
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        id={data?.id}
+      />
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, md: 8 }}>
+            <Box
+              sx={{
+                height: 300,
+                width: '100%',
+                overflow: 'block',
+                borderRadius: 1,
+              }}
+            >
+              <Image
+                height={300}
+                width={400}
+                src={data?.profilePhoto}
+                alt="User Photo"
               />
-            )}
-          </Box>
+            </Box>
+            <Box my={3}>
+              {loading ? (
+                <p>Uploading...</p>
+              ) : (
+                <AutoFileUploader
+                  name="file"
+                  label="Choose Your Profile Photo"
+                  icon={<CloudUploadIcon />}
+                  onFileUpload={fileUploadHandler}
+                  variant="text"
+                />
+              )}
+            </Box>
+            <Button
+              fullWidth
+              endIcon={<ModeEditIcon />}
+              onClick={() => setIsModalOpen(true)}
+            >
+              Edit Profile
+            </Button>
+          </Grid>
           <Grid size={{ xs: 12, md: 8 }}>
             <DoctorInformation data={data} />
           </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
 
