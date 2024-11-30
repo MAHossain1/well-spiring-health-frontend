3322817570de6161663c95673ba36e6d89160b88
MAHossain1 <mahpro110@gmail.com>
2024-11-30 10:55:11 +0600

    navbar update and not found page update.

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,51 +1,82 @@
 'use client';
 
-import { Box, Container, Stack, Typography } from '@mui/material';
+import useUserInfo from '@/hooks/useUserInfo';
+import { logoutUser } from '@/services/actions/logoutUser';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 const AuthButton = dynamic(
   () => import('@/components/UI/AuthButton/AuthButton'),
   { ssr: false }
 );
 
 const Navbar = () => {
+  const userInfo = useUserInfo();
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    logoutUser(router);
+  };
+
   return (
-    <Container>
-      <Stack
-        py={2}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Typography component={Link} href="/" variant="h4" fontWeight={600}>
-          Well
-          <Box component="span" color="primary.main">
-            Spring
-          </Box>{' '}
-          Health
-        </Typography>
+    <Box
+      sx={{
+        bgcolor: 'primary.main',
+      }}
+    >
+      <Container>
+        <Stack
+          py={2}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          {/* <Typography component={Link} href="/" variant="h4" fontWeight={600}>
+            Well
+            <Box component="span" color="primary.main">
+              Spring
+            </Box>{' '}
+            Health
+          </Typography> */}
 
-        <Stack direction="row" justifyContent="space-between" gap={4}>
-          <Typography component={Link} href="/consultation">
-            Consultation
+          <Typography variant="h4" component={Link} href="/" fontWeight={600}>
+            Well
+            <Box component="span" color="#ffffff">
+              Spring
+            </Box>{' '}
+            Health
           </Typography>
-          <Typography component={Link} href="/heath-plans">
-            Health Plans
-          </Typography>
-          <Typography component={Link} href="/medicine">
-            Medicine
-          </Typography>
-          <Typography component={Link} href="/diagnostics">
-            Diagnostics
-          </Typography>
-          <Typography component={Link} href="/ngos">
-            NGOs
-          </Typography>
+
+          <Stack direction="row" justifyContent="space-between" gap={4}>
+            <Typography component={Link} href="/consultation" color="#ffffff">
+              Consultation
+            </Typography>
+
+            <Typography color="#ffffff">Diagnostics</Typography>
+            <Typography component={Link} href="/doctors" color="#ffffff">
+              Doctors
+            </Typography>
+
+            {userInfo?.userId ? (
+              <Typography component={Link} href="/dashboard" color="#ffffff">
+                Dashboard
+              </Typography>
+            ) : null}
+          </Stack>
+
+          {userInfo?.userId ? (
+            <Button color="error" onClick={handleLogOut} sx={{ boxShadow: 0 }}>
+              Logout
+            </Button>
+          ) : (
+            <Button component={Link} href="/login">
+              Login
+            </Button>
+          )}
         </Stack>
-
-        <AuthButton />
-      </Stack>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
